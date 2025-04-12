54641cd2db391a82b2edaf150e10fcdabe99a35d
pablopasquim <pablo.pasquim7@gmail.com>
2025-04-12 17:26:38 -0300

    🎉 Initial Commit for playground 🎉

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func main() {
 
 	fmt.Println(calcSoma(1, 2))
 	fmt.Println(Calc.Sum(1, 2))
+	fmt.Println(calcSub(1, 2))
 
 }
 
 func calcSoma(a int, b int) int {
 	return a + b
+}
+
+func calcSub(a int, b int) int {
+	return a - b
 }
